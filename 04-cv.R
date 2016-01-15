a247e478bd5ba3f89b2633ead9cf5f30936b4a85
Siyu Sun <sunsiyu.tud@gmail.com>
2016-01-15 23:56:25 +0100

    add cross-validation template

---
+++ 04-cv.R
@@ -0,0 +1,58 @@
+# ===================
+# LOAD PACKAGES
+# ===================
+library(boot)
+library(caret)
+library(mlbench)
+
+# ===================
+# CROSS VALIDATION
+# ===================
+# leave-one-out and 6-fold cross-validation prediction error for 
+# the mammals data set.
+data(mammals, package = "MASS")
+mammals.glm <- glm(log(brain) ~ log(body), data = mammals)
+
+cv.err <- cv.glm(mammals, mammals.glm)$delta
+cv.err.6 <- cv.glm(mammals, mammals.glm, K = 6)$delta
+# As this is a linear model we could calculate the leave-one-out 
+# cross-validation estimate without any extra model-fitting.
+muhat <- fitted(mammals.glm)
+mammals.diag <- glm.diag(mammals.glm)
+(cv.err <- mean((mammals.glm$y - muhat)^2/(1 - mammals.diag$h)^2))
+
+
+# leave-one-out and 11-fold cross-validation prediction error for 
+# the nodal data set.  Since the response is a binary variable an
+# appropriate cost function is
+cost <- function(r, pi = 0) mean(abs(r-pi) > 0.5)
+
+nodal.glm <- glm(r ~ stage+xray+acid, binomial, data = nodal)
+(cv.err <- cv.glm(nodal, nodal.glm, cost, K = nrow(nodal))$delta)
+(cv.11.err <- cv.glm(nodal, nodal.glm, cost, K = 11)$delta)
+
+
+
+
+# =============================
+# CROSS VALIDATION WITH CARET
+# =============================
+data(Sonar)
+inTrain <- createDataPartition(Sonar$Class, p = .75, list = FALSE)
+training <- Sonar[inTrain, ]
+testing  <- Sonar[-inTrain, ]
+
+
+fitControl <- trainControl(## 10-fold CV
+                           method = "repeatedcv",
+                           number = 10, 
+                           ## repeated ten times
+                           repeats = 10)
+
+gbmFit1 <- train(Class ~ ., 
+                 data = training,
+                 method = "gbm",
+                 trControl = fitControl,
+                 ## This last option is actually one
+                 ## for gbm() that passes through
+                 verbose = FALSE)
