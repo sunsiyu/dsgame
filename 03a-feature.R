215b7961b00a27fbd5b77920dcec34b832d37dfc
Siyu Sun <sunsiyu.tud@gmail.com>
2016-03-31 23:52:48 +0200

    add generic feature generation strategy

---
+++ 03a-feature.R
@@ -0,0 +1,17 @@
+source("00-lib.R")
+
+# Some thoughts 2016-03-31
+# From semi-clean data sources to clean features
+#
+# The Mapping Mechanism is finite
+# The output is always in tabular form (e.g. matrix, data.frame, data.table etc.)
+# elements
+# - unit data structure - smallest data structure for each observation / row
+# - data types (atomic or non-atomic, etc.)
+# - consistency of above two between observations
+# - missing values strategies
+
+
+
+
+
